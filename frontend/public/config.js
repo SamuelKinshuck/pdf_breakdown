3befd2aed1ec613186b2f97e97990e2260087e7b
Samuel Kinshuck <samuel.kinshuck@gad.gov.uk>
2025-09-29 13:02:32 +0100

    fix typo

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -10,6 +10,6 @@ x = "http://gad-hosting:8316/"
 var BACKEND_URL = x;
 
 // public/config.js
-window.BACKEND_URL = "http://localhost:4005";
+window.BACKEND_URL = BACKEND_URL;
 // optional alias if you want it
 window.API_BASE = window.BACKEND_URL;
